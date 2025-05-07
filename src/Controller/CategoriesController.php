0af0a320de21157cdcf5dd0f3de1e564b9b151fe
nexus806 <antonin.merlomeyffren@ynov.com>
2025-05-07 12:12:07 +0200

    création d'un controller pour les catégorie avec une page temporaire et redirection vers cette page quand on clique sur le bouton "Catégories" de l'accueil

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -5,15 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\CategoriesRepository;
+
 
 #[Route('/categories', name: 'categories_')]
 final class CategoriesController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(CategoriesRepository $categoriesController): Response
     {
         return $this->render('categories/index.html.twig', [
-            'controller_name' => 'profil user',
+            'controller_name' => 'categories',
+            'categories' => $categoriesController->findBy([], ['id' => 'ASC']),
+
         ]);
     }
 }
