d0f2f1b78273e75e5d6074f5df4b061f70da9376
nexus806 <antonin.merlomeyffren@ynov.com>
2025-05-07 11:01:26 +0200

    ajout d'un html temporaire et affichage des catégories dans la partie catégorie

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,17 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController{
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(CategoriesRepository $categoriesRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'categories' => $categoriesRepository->findBy([],['id' => 'ASC']),
         ]);
     }
 }
