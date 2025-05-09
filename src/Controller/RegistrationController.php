6b64fe08c9cc95acd2800950c165ce0d1c146b96
MayVCODE <mayssou839@gmail.com>
2025-05-09 19:22:58 +0200

    phpcbf

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -16,8 +16,7 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, 
-    EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
