a2494034bfd3c4f4bbaad10f99e7fd6389c4857c
Antonin Merlo Meyffren <antonin.merlomeyffren@ynov.com>
2025-05-11 20:26:47 +0200

    affichage des post en fonction de leur catégorie

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\PostsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\CategoriesRepository;
 
@@ -17,6 +19,23 @@ final class CategoriesController extends AbstractController
             'controller_name' => 'categories',
             'categories' => $categoriesController->findBy([], ['id' => 'ASC']),
 
+        ]);
+    }
+    #[Route('/{id}', name: 'posts')]
+    public function showByCategorie(int $id, CategoriesRepository $catRepo, PostsRepository $postRepo): Response
+    {
+        dump($id);
+        $categorie = $catRepo->find($id);
+
+        if (!$categorie) {
+            throw $this->createNotFoundException('Catégorie non trouvée');
+        }
+
+        $posts = $postRepo->findBy(['cat' => $categorie], ['id' => 'DESC']);
+
+        return $this->render('categories/show.html.twig', [
+            'categorie' => $categorie,
+            'posts' => $posts,
         ]);
     }
 }
