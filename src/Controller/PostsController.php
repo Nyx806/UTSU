f06faa20432117d39ab43c6e5a8778520909590e
Antonin Merlo Meyffren <antonin.merlomeyffren@ynov.com>
2025-05-20 12:05:45 +0200

    mise en place d'un formulaire fonctionnelle

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -8,15 +8,27 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Posts;
 use App\Form\PostsFromType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/posts', name: 'posts_')]
 final class PostsController extends AbstractController
 {
     #[Route('/ajout', name: 'ajout')]
-    public function ajout(): Response
+    public function ajout(Request $request, EntityManagerInterface $em): Response
     {
         $posts = new Posts();
         $form = $this->createForm((PostsFromType::class), $posts);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $posts = $form->getData();
+            $posts->setDate(new \DateTimeImmutable());
+            $em->persist($posts);
+            $em->flush();
+
+            return $this->redirectToRoute('home_index');
+        }
 
         return $this->render('posts/ajout.html.twig', [
             'controller_name' => 'PostsController',
@@ -32,5 +44,4 @@ final class PostsController extends AbstractController
             'post' => $postsRepository->find($id),
         ]);
     }
-
 }
--- src/Form/PostsFromType.php
+++ src/Form/PostsFromType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Categories;
 use App\Entity\Posts;
 use App\Entity\User;
+use Dom\Entity;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,10 +18,21 @@ class PostsFromType extends AbstractType
         $builder
             ->add('title')
             ->add('contenu')
-            ->add('date', null, [
+            /* ->add('date', null, [
                 'widget' => 'single_text',
+            ]) */
+            ->add('cat', EntityType::class, [
+                'class' => Categories::class,
+                'choice_label' => 'name',
+                'label' => 'Catégorie'
             ])
-            ->add('photo')
+            ->add('userID', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'username',
+                'label' => 'Utilisateur'
+            ])
+            //->add('photo')
+
         ;
     }
 
