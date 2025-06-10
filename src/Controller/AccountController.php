dced5cad01fe683d238082b01fa2cbdda8bbe7ae
nexus806 <antonin.merlomeyffren@ynov.com>
2025-06-10 17:20:34 +0200

    modification du profil

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,15 +3,22 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\AccountFormType;
 
 #[Route('/account', name: 'account_')]
 final class AccountController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(UserRepository $userRepository): Response
+    public function index(UserRepository $userRepository,
+    Request $request,
+    EntityManagerInterface $em
+    ): Response
     {
         // Récupérer l'utilisateur connecté
         $user = $this->getUser();
@@ -21,9 +28,37 @@ final class AccountController extends AbstractController
             return $this->redirectToRoute('app_login'); // Rediriger vers la page de connexion si non connecté
         }
 
+        // Créer le formulaire de compte
+        $accountForm = $this->createForm(AccountFormType::class, $user);
+        $accountForm->handleRequest($request);
+        if ($accountForm->isSubmitted() && $accountForm->isValid()) {
+            // Récupérer les données du formulaire
+            $data = $accountForm->getData();
+
+            // Vérifier si une nouvelle image de profil a été téléchargée
+            $file = $accountForm->get('pp_img')->getData();
+            if ($file) {
+                // Générer un nom de fichier unique
+                $fileName = md5(uniqid()) . '.' . $file->guessExtension();
+                // Déplacer le fichier vers le répertoire public/uploads/pp
+                $file->move($this->getParameter('kernel.project_dir') . '/public/uploads/pp', $fileName);
+                // Mettre à jour l'image de profil de l'utilisateur
+                $data->setPpImg($fileName);
+            }
+
+            // Mettre à jour les informations de l'utilisateur
+            $em->persist($data);
+            $em->flush();
+
+            // Rediriger vers la page d'accueil ou afficher un message de succès
+            return $this->redirectToRoute('home_index');
+        }
         return $this->render('account/index.html.twig', [
             'controller_name' => 'AccountController',
             'user' => $user, // Passer l'utilisateur connecté à la vue
+            'accountForm' => $accountForm->createView(),
         ]);
     }
+
+
 }
---
+++ src/Form/AccountFormType.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+
+class AccountFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez entrer un email']),
+                    new Length([
+                        'max' => 180,
+                        'maxMessage' => 'L\'email ne peut pas dépasser {{ limit }} caractères',
+                    ]),
+                ],
+            ])
+            ->add('username', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez entrer un nom d\'utilisateur']),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Votre nom d\'utilisateur doit contenir au moins {{ limit }} caractères',
+                        'max' => 255,
+                        'maxMessage' => 'Votre nom d\'utilisateur ne peut pas dépasser {{ limit }} caractères',
+                    ]),
+                ],
+            ])
+            ->add('pp_img', FileType::class, [
+                'required' => false,
+                'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/gif',
+                        ],
+                        'mimeTypesMessage' => 'Veuillez télécharger une image valide (JPEG, PNG, GIF)',
+                    ]),
+                ],
+                'attr' => [
+                    'accept' => 'image/*',
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
